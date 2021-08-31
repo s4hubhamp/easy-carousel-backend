9700e6c4ccdf6766b6acbb1645cdd33367b80242
Shubham <pwrs.0007@gmail.com>
2021-08-31 20:49:24 +0530

    restructured photos that'll sent to frontend

--- api/controllers/photoscontroller.js
+++ api/controllers/photoscontroller.js
@@ -6,7 +6,7 @@ exports.getPhotosByCategory = async (req, res, next) => {
     const { qty } = req.query;
 
     const response = await fetch(
-      `https://api.unsplash.com/search/photos?client_id=5F6xRIdUuQBX1556iGYD-5b-Fl8xP5wbiJk9IewNzTsQQ&query=${category}&orientation=squarish&per_page=${
+      `https://api.unsplash.com/search/photos?client_id=5F6xRIdUuQBX1556iGYD-5b-Fl8xP5wbiJk9IewNzTs&query=${category}&orientation=squarish&per_page=${
         qty || 5
       }`
     );
@@ -19,8 +19,10 @@ exports.getPhotosByCategory = async (req, res, next) => {
     }
 
     const photos = data.results.map((searchResult) => ({
-      url: searchResult.urls.regular,
+      regular: searchResult.urls.regular,
       id: searchResult.id,
+      description: searchResult.description,
+      thumb: searchResult.urls.thumb,
     }));
 
     return res.json({ photos });
